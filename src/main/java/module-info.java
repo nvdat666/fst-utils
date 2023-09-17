1ee037675e0ae08bd6295ec3663e365ea42bb9fb
nvdat666 <nvdat666@gmail.com>
2023-09-17 23:24:26 +0700

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,16 +4,15 @@ module com.example.fstutils {
     requires javafx.web;
 
     requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
+//    requires com.dlsc.formsfx;
+//    requires net.synedra.validatorfx;
+//    requires org.kordamp.ikonli.javafx;
+//    requires org.kordamp.bootstrapfx.core;
 //    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
+//    requires com.almasb.fxgl.all;
     requires json.simple;
     requires poi.ooxml;
     requires poi;
-    requires org.json;
     requires org.fxmisc.richtext;
     requires java.desktop;
 
